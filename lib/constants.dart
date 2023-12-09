c59920a082fd4bc7d98d55fa713d50a7caee1870
dacbac2702 <bac27022002@gmail.com>
2023-12-09 19:30:28 +0700

    Add file constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+// Store my colors and stuff here
+// Import the material package
+import 'package:flutter/material.dart';
+
+// Set the color for correct answer
+const Color correct = Color(0xFF00D756);
+
+// Set the color for incorrect answer
+const Color incorrect = Color(0xFFD70000);
+
+// Set a neutral color
+const Color neutral = Color(0xFFF9F9F9);
+
+// Set some background color
+const Color background = Color(0xFF0957B0);
